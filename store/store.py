22748a53832812534ee09bb4841c360ab611ff34
Pete Belknap <pete@fastrak.app>
2023-05-24 15:34:15 -0700

    push mkdir into mmm

--- store/store.py
+++ store/store.py
@@ -6,9 +6,9 @@ from datetime import datetime
 from impl.lightweight_mmm.lightweight_mmm.utils import save_model as lwmmm_save_model
 
 
-def generate_results_dirname(data_dir, dirname_fixed):
+def make_results_dir(data_dir, dirname_fixed):
     """
-    generate a unique directory name for a given customer name
+    create a directory with a unique name for this mmm run
 
     :param data_dir: directory prefix
     :param dirname_fixed: customer name suitable for including in a pathname
@@ -18,12 +18,16 @@ def generate_results_dirname(data_dir, dirname_fixed):
     yyyymmdd = now_date.date().strftime("%Y-%m-%d")
     seconds_since_midnight = now_date.hour * 3600 + now_date.minute * 60 + now_date.second
     hextoken = secrets.token_hex(4)
-    return os.path.join(
+    results_dir = os.path.join(
         data_dir,
         "results",
         dirname_fixed,
         f"{yyyymmdd}-{seconds_since_midnight}-{hextoken}"
     )
+    print(f"Creating directory '{results_dir}' for output")
+    os.makedirs(results_dir, exist_ok=False)
+
+    return results_dir
 
 
 def save_model(mmm, results_dir):
