9eeeb38f500621aabb547d3dc489e3d73190820b
Hourann Bosci <azza-bazoo@users.noreply.github.com>
2023-06-27 18:44:29 +0800

    output fit params as yaml rather than text, and include the random seed, for reproducibility

--- fit/fit.py
+++ fit/fit.py
@@ -1,8 +1,10 @@
 from ..impl.lightweight_mmm.lightweight_mmm import lightweight_mmm
+from ..impl.lightweight_mmm.lightweight_mmm.utils import get_time_seed
 
 from ..constants import constants
 
 import os
+import yaml
 
 
 # noinspection GrazieInspection
@@ -46,37 +48,39 @@ def fit_lightweight_mmm(
     else:
         extra_features = data_to_fit.extra_features_train_scaled
 
-    seasonality_frequency = 365 if data_to_fit.time_granularity == constants.GRANULARITY_DAILY else 52
+    fit_params = {
+        "model_name": model_name,
+        "degrees_seasonality": degrees_seasonality,
+        "number_warmup": number_warmup,
+        "number_samples": number_warmup,
+        "media_prior": data_to_fit.media_priors_scaled.tolist(),
+        "target_is_log_scale": data_to_fit.target_is_log_scale,
+    }
+
+    fit_params["seasonality_frequency"] = 365 if data_to_fit.time_granularity == constants.GRANULARITY_DAILY else 52
 
     if weekday_seasonality is None:
-        weekday_seasonality_touse = True if data_to_fit.time_granularity == constants.GRANULARITY_DAILY else False
+        fit_params["weekday_seasonality"] = True if data_to_fit.time_granularity == constants.GRANULARITY_DAILY else False
     else:
-        weekday_seasonality_touse = weekday_seasonality
+        fit_params["weekday_seasonality"] = weekday_seasonality
 
     # number_chains=1 because my laptop has only one CPU (jax.local_device_count())
-    number_chains = 1
+    fit_params["number_chains"] = 1
 
-    with open(os.path.join(results_dir, "fit_params.txt"), "w") as output_file:
-        output_file.write(f"model_name={model_name}\n")
-        output_file.write(f"degrees_seasonality={degrees_seasonality}\n")
-        output_file.write(f"seasonality_frequency={seasonality_frequency}\n")
-        output_file.write(f"weekday_seasonality_touse={weekday_seasonality_touse}\n")
-        output_file.write(f"media_prior={data_to_fit.media_priors_scaled}\n")
-        output_file.write(f"number_warmup={number_warmup}\n")
-        output_file.write(f"number_samples={number_samples}\n")
-        output_file.write(f"number_chains={number_chains}\n")
+    # manually generate a seed in the same way as lightweight mmm's
+    # fit(), and store it for future reproducibility
+    fit_params["seed"] = get_time_seed()
+
+    with open(os.path.join(results_dir, "fit_params.yaml"), "w") as output_file:
+        yaml.dump(fit_params, output_file, default_flow_style=False)
+
+    # remove parameter(s) that we want to write, but don't want in fit()
+    del fit_params["model_name"]
 
     mmm.fit(media=data_to_fit.media_data_train_scaled,
-            degrees_seasonality=degrees_seasonality,
-            seasonality_frequency=seasonality_frequency,
-            weekday_seasonality=weekday_seasonality_touse,
             media_names=data_to_fit.media_names,
             extra_features=extra_features,
-            media_prior=data_to_fit.media_priors_scaled,
             target=data_to_fit.target_train_scaled,
-            target_is_log_scale=data_to_fit.target_is_log_scale,
-            number_warmup=number_warmup,
-            number_samples=number_samples,
-            number_chains=number_chains)
+            **fit_params)
 
     return mmm
