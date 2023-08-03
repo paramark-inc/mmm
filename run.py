ca4d2acb4110029a39ecc34c6cf9b5b694b16d6a
Hourann Bosci <azza-bazoo@users.noreply.github.com>
2023-08-03 11:40:17 -0700

    new run file to import base_driver and set sys.path appropriately

---
+++ run.py
@@ -0,0 +1,19 @@
+import click
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "impl", "lightweight_mmm"))
+
+from base_driver import MMMBaseDriver
+
+
+@click.command()
+@click.option("-c", "--config_filename", required=True, help="yaml config file")
+@click.option("-f", "--input_filename", required=True, help="input data filename")
+def main(config_filename: str, input_filename: str):
+    driver = MMMBaseDriver()
+    driver.main(config_filename, input_filename)
+
+
+if __name__ == "__main__":
+    main()
