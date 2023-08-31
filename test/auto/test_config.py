1d7ee4da32f3eda4da03f3c7cf69067c7ac96f53
Hourann Bosci <azza-bazoo@users.noreply.github.com>
2023-08-31 15:19:23 +0800

    minimal config file for testing, and a test for load_config that uses it

---
+++ test/auto/test_config.py
@@ -0,0 +1,24 @@
+from datetime import date
+import os
+import unittest
+
+from base_driver.config import load_config
+
+
+class ConfigTest(unittest.TestCase):
+    def test_load_config(self):
+        config_filename = os.path.join(os.path.dirname(__file__), "..", "test.yaml")
+
+        raw_config, config = load_config(config_filename)
+
+        self.assertIn("\nweekday_seasonality: null\n", raw_config)
+
+        self.assertEqual(config["seed"], 1)
+
+        self.assertDictEqual(
+            config["data_rows"],
+            {
+                "start_date": date(year=2023, month=1, day=1),
+                "end_date": date(year=2023, month=1, day=10),
+            },
+        )
