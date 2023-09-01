4d7deb4d83133c52294bf6f2d518f396400f8a8e
Hourann Bosci <azza-bazoo@users.noreply.github.com>
2023-09-01 17:53:43 +0800

    update tests to suit 6b7d96736adcd8fb8799c2a2070d3d1a66c01c84, and split up old "model" test to just be for InputData

--- test/auto/test_data_to_fit.py
+++ test/auto/test_data_to_fit.py
@@ -1,31 +1,84 @@
 import numpy as np
 import os
+import pandas as pd
+
 import unittest
+from numpy.testing import assert_array_equal, assert_array_almost_equal
+from pandas.testing import assert_series_equal
 
 from base_driver.config import load_config
+from mmm.constants import constants
 from mmm.parser.csv import parse_csv_generic
 from mmm.transform.transform import transform_input_generic
 
-from mmm.data import DataToFit
+from .test_input_data import InputDataTest
+from mmm.data import DataToFit, InputData
 
 
 class DataToFitTest(unittest.TestCase):
-    def test_data_to_fit_to_dict(self):
-        config_filename = os.path.join(os.path.dirname(__file__), "..", "test.yaml")
-        input_filename = os.path.join(os.path.dirname(__file__), "..", "test.csv")
+    config_filename = os.path.join(os.path.dirname(__file__), "..", "test.yaml")
+    csv_filename = os.path.join(os.path.dirname(__file__), "..", "test.csv")
+    gz_filename = os.path.join(os.path.dirname(__file__), "..", "test_data_to_fit.gz")
 
-        _, config = load_config(config_filename)
-        data_dict = parse_csv_generic(input_filename, config)
+    # noinspection PyMethodMayBeStatic
+    def test_data_to_fit_from_input_data(self):
+        input_data = InputDataTest.generate_test_input_data()
+        data_to_fit = DataToFit.from_input_data(input_data=input_data, config={})
+        assert_array_equal(data_to_fit.date_strs, input_data.date_strs)
+
+        data_to_fit_media_data = np.vstack(
+            (data_to_fit.media_data_train_scaled, data_to_fit.media_data_test_scaled)
+        )
+        data_to_fit_media_data_unscaled = data_to_fit.media_scaler.inverse_transform(
+            data_to_fit_media_data
+        )
+        assert_array_almost_equal(data_to_fit_media_data_unscaled, input_data.media_data, decimal=3)
+
+        data_to_fit_media_costs_unscaled = data_to_fit.media_costs_scaler.inverse_transform(
+            data_to_fit.media_costs_scaled
+        )
+        assert_array_almost_equal(
+            data_to_fit_media_costs_unscaled, input_data.media_costs, decimal=3
+        )
+
+        data_to_fit_extra_features_data = np.vstack(
+            (data_to_fit.extra_features_train_scaled, data_to_fit.extra_features_test_scaled)
+        )
+        data_to_fit_extra_features_data_unscaled = (
+            data_to_fit.extra_features_scaler.inverse_transform(data_to_fit_extra_features_data)
+        )
+        assert_array_almost_equal(
+            data_to_fit_extra_features_data_unscaled, input_data.extra_features_data, decimal=3
+        )
+
+        data_to_fit_target_data = np.hstack(
+            (data_to_fit.target_train_scaled, data_to_fit.target_test_scaled)
+        )
+        data_to_fit_target_data_unscaled = data_to_fit.target_scaler.inverse_transform(
+            data_to_fit_target_data
+        )
+        assert_array_almost_equal(
+            data_to_fit_target_data_unscaled, input_data.target_data, decimal=3
+        )
+
+    def test_data_to_fit_with_train_only(self):
+        input_data = InputDataTest.generate_test_input_data()
+
+        data_to_fit = DataToFit.from_input_data(
+            input_data=input_data, config={"train_test_ratio": 1.0}
+        )
+
+    def test_data_to_fit_to_dict(self):
+        _, config = load_config(self.config_filename)
+        data_dict = parse_csv_generic(self.csv_filename, config)
         input_data = transform_input_generic(data_dict, config)
 
-        data_to_fit = DataToFit.from_input_data(input_data=input_data)
+        data_to_fit = DataToFit.from_input_data(input_data=input_data, config=config)
 
         output = data_to_fit.to_dict()
 
-        np.testing.assert_array_almost_equal(
-            output["data"]["media_priors_scaled"], [1.5714285, 0.42857143]
-        )
-        np.testing.assert_array_equal(
+        assert_array_almost_equal(output["data"]["media_priors_scaled"], [1.5714285, 0.42857143])
+        assert_array_equal(
             output["display"]["date_strs"], [f"2023-01-{n:02}" for n in range(1, 11)]
         )
 
@@ -45,12 +98,11 @@ class DataToFitTest(unittest.TestCase):
         self.assertEqual(len(data_to_fit.__dict__), dict_count)
 
     def test_data_to_fit_from_gz_file(self):
-        filename = os.path.join(os.path.dirname(__file__), "..", "test_data_to_fit.gz")
-        data_to_fit = DataToFit.from_file(filename)
+        data_to_fit = DataToFit.from_file(self.gz_filename)
 
         self.assertIsInstance(data_to_fit, DataToFit)
 
-        np.testing.assert_array_almost_equal(
+        assert_array_almost_equal(
             data_to_fit.target_train_scaled,
             [
                 0.18181819,
@@ -65,7 +117,140 @@ class DataToFitTest(unittest.TestCase):
             ],
         )
 
-        np.testing.assert_array_almost_equal(
+        assert_array_almost_equal(
             data_to_fit.target_test_scaled,
             [1.8181819],
+        )
+
+    def test_data_to_fit_to_data_frame_from_input_data(self):
+        input_data = InputDataTest.generate_test_input_data()
+
+        data_to_fit = DataToFit.from_input_data(input_data=input_data, config={})
+        data_to_fit_media_data = np.vstack(
+            (data_to_fit.media_data_train_scaled, data_to_fit.media_data_test_scaled)
+        )
+        data_to_fit_costs_by_row_data = np.vstack(
+            (
+                data_to_fit.media_costs_by_row_train_scaled,
+                data_to_fit.media_costs_by_row_test_scaled,
+            )
+        )
+
+        data_to_fit_extra_features_data = np.vstack(
+            (data_to_fit.extra_features_train_scaled, data_to_fit.extra_features_test_scaled)
+        )
+        data_to_fit_target_data = np.hstack(
+            (data_to_fit.target_train_scaled, data_to_fit.target_test_scaled)
+        )
+
+        # unscaled = False
+        per_observation_df, per_channel_df = data_to_fit.to_data_frame()
+
+        self.assertEqual(100, per_observation_df.shape[0])
+        self.assertEqual(2 + 2 + 3 + 1, per_observation_df.shape[1])
+
+        assert_array_almost_equal(
+            per_observation_df["Channel1 volume"], data_to_fit_media_data[:, 0], decimal=3
+        )
+        assert_array_almost_equal(
+            per_observation_df["Channel2 volume"], data_to_fit_media_data[:, 1], decimal=3
+        )
+        assert_array_almost_equal(
+            per_observation_df["Channel1 cost"], data_to_fit_costs_by_row_data[:, 0], decimal=3
+        )
+        assert_array_almost_equal(
+            per_observation_df["Channel2 cost"], data_to_fit_costs_by_row_data[:, 1], decimal=3
+        )
+
+        self.assertAlmostEqual(
+            data_to_fit_media_data[1, 1], per_observation_df.loc["2022-01-02", "Channel2 volume"]
+        )
+
+        assert_array_almost_equal(
+            per_observation_df["Feature1"], data_to_fit_extra_features_data[:, 0], decimal=3
+        )
+        assert_array_almost_equal(
+            per_observation_df["Feature2"], data_to_fit_extra_features_data[:, 1], decimal=3
+        )
+        assert_array_almost_equal(
+            per_observation_df["Feature3"], data_to_fit_extra_features_data[:, 2], decimal=3
+        )
+        assert_array_almost_equal(per_observation_df["Target"], data_to_fit_target_data, decimal=3)
+
+        self.assertEqual(2, per_channel_df.shape[0])
+        assert_array_almost_equal(per_channel_df["Cost"], data_to_fit.media_costs_scaled, decimal=3)
+
+        # unscaled = True
+        per_observation_df, per_channel_df = data_to_fit.to_data_frame(unscaled=True)
+
+        self.assertEqual(100, per_observation_df.shape[0])
+        self.assertEqual(2 + 2 + 3 + 1, per_observation_df.shape[1])
+
+        assert_array_almost_equal(
+            per_observation_df["Channel1 volume"], input_data.media_data[:, 0], decimal=3
+        )
+        assert_array_almost_equal(
+            per_observation_df["Channel2 volume"], input_data.media_data[:, 1], decimal=3
+        )
+        assert_array_almost_equal(
+            per_observation_df["Channel1 cost"], input_data.media_costs_by_row[:, 0], decimal=2
+        )
+        assert_array_almost_equal(
+            per_observation_df["Channel2 cost"], input_data.media_costs_by_row[:, 1], decimal=2
+        )
+
+        self.assertAlmostEqual(
+            input_data.media_data[1, 1],
+            per_observation_df.loc["2022-01-02", "Channel2 volume"],
+            places=3,
+        )
+
+        assert_array_almost_equal(
+            per_observation_df["Feature1"], input_data.extra_features_data[:, 0], decimal=3
+        )
+        assert_array_almost_equal(
+            per_observation_df["Feature2"], input_data.extra_features_data[:, 1], decimal=3
+        )
+        assert_array_almost_equal(
+            per_observation_df["Feature3"], input_data.extra_features_data[:, 2], decimal=3
+        )
+        assert_array_almost_equal(per_observation_df["Target"], input_data.target_data, decimal=3)
+
+        self.assertEqual(2, per_channel_df.shape[0])
+        assert_array_almost_equal(per_channel_df["Cost"], input_data.media_costs, decimal=3)
+
+    def test_data_to_fit_to_data_frame_from_gz(self):
+        data_to_fit = DataToFit.from_file(self.gz_filename)
+
+        # omit last row of CSV because it's excluded in parser by date range
+        csv_df = pd.read_csv(self.csv_filename)[:-1]
+
+        per_observation_df, per_channel_df = data_to_fit.to_data_frame(unscaled=True)
+
+        self.assertEqual(csv_df["Spend1"].sum(), per_channel_df["Cost"].loc["Channel 1"])
+
+        assert_series_equal(
+            pd.to_datetime(csv_df["Date"]),
+            per_observation_df.index.to_series(),
+            check_index=False,
+            check_names=False,
+        )
+
+        assert_series_equal(
+            csv_df["Impressions1"].astype(float),
+            per_observation_df["Channel 1 volume"],
+            check_index=False,
+            check_names=False,
+        )
+        assert_series_equal(
+            csv_df["Spend2"].astype(float),
+            per_observation_df["Channel 2 cost"],
+            check_index=False,
+            check_names=False,
+        )
+        assert_series_equal(
+            csv_df["Target"].astype(float),
+            per_observation_df["Target"],
+            check_index=False,
+            check_names=False,
         )
--- test/auto/test_input_data.py
+++ test/auto/test_input_data.py
@@ -8,9 +8,9 @@ from mmm.constants import constants
 from mmm.data import InputData, DataToFit
 
 
-class ModelTestCase(unittest.TestCase):
+class InputDataTest(unittest.TestCase):
     @staticmethod
-    def _generate_test_input_data(observations=100):
+    def generate_test_input_data(observations=100):
         date_strs = [
             (date.fromisoformat("2022-01-01") + timedelta(days=idx)).strftime("%-m/%-d/%Y")
             for idx in range(observations)
@@ -45,147 +45,27 @@ class ModelTestCase(unittest.TestCase):
             target_name="Target",
         )
 
-    # noinspection PyMethodMayBeStatic
-    def test_make_data_to_fit(self):
-        input_data = ModelTestCase._generate_test_input_data()
-        data_to_fit = DataToFit.from_input_data(input_data)
-        assert_array_equal(data_to_fit.date_strs, input_data.date_strs)
-
-        data_to_fit_media_data = np.vstack(
-            (data_to_fit.media_data_train_scaled, data_to_fit.media_data_test_scaled)
-        )
-        data_to_fit_media_data_unscaled = data_to_fit.media_scaler.inverse_transform(
-            data_to_fit_media_data
-        )
-        assert_array_almost_equal(data_to_fit_media_data_unscaled, input_data.media_data, decimal=3)
-
-        data_to_fit_media_costs_unscaled = data_to_fit.media_costs_scaler.inverse_transform(
-            data_to_fit.media_costs_scaled
-        )
-        assert_array_almost_equal(
-            data_to_fit_media_costs_unscaled, input_data.media_costs, decimal=3
+    def test_input_data_trivial_create(self):
+        input_data = InputData(
+            date_strs=np.array(["1999-12-31"]),
+            time_granularity=constants.GRANULARITY_DAILY,
+            media_data=np.array([[1.0]]),
+            media_costs=np.array([3.0]),
+            media_costs_by_row=np.array([[3.0]]),
+            media_priors=np.array([3.0]),
+            media_names=["foo"],
+            extra_features_data=np.array([[4.0]]),
+            extra_features_names=["bar"],
+            target_data=np.array([9.0]),
+            target_is_log_scale=False,
+            target_name="baz",
         )
 
-        data_to_fit_extra_features_data = np.vstack(
-            (data_to_fit.extra_features_train_scaled, data_to_fit.extra_features_test_scaled)
-        )
-        data_to_fit_extra_features_data_unscaled = (
-            data_to_fit.extra_features_scaler.inverse_transform(data_to_fit_extra_features_data)
-        )
-        assert_array_almost_equal(
-            data_to_fit_extra_features_data_unscaled, input_data.extra_features_data, decimal=3
-        )
-
-        data_to_fit_target_data = np.hstack(
-            (data_to_fit.target_train_scaled, data_to_fit.target_test_scaled)
-        )
-        data_to_fit_target_data_unscaled = data_to_fit.target_scaler.inverse_transform(
-            data_to_fit_target_data
-        )
-        assert_array_almost_equal(
-            data_to_fit_target_data_unscaled, input_data.target_data, decimal=3
-        )
-
-    def test_to_data_frame(self):
-        input_data = ModelTestCase._generate_test_input_data()
-
-        data_to_fit = DataToFit.from_input_data(input_data)
-        data_to_fit_media_data = np.vstack(
-            (data_to_fit.media_data_train_scaled, data_to_fit.media_data_test_scaled)
-        )
-        data_to_fit_costs_by_row_data = np.vstack(
-            (
-                data_to_fit.media_costs_by_row_train_scaled,
-                data_to_fit.media_costs_by_row_test_scaled,
-            )
-        )
-
-        data_to_fit_extra_features_data = np.vstack(
-            (data_to_fit.extra_features_train_scaled, data_to_fit.extra_features_test_scaled)
-        )
-        data_to_fit_target_data = np.hstack(
-            (data_to_fit.target_train_scaled, data_to_fit.target_test_scaled)
-        )
-
-        # unscaled = False
-        per_observation_df, per_channel_df = data_to_fit.to_data_frame()
-
-        self.assertEqual(100, per_observation_df.shape[0])
-        self.assertEqual(2 + 2 + 3 + 1, per_observation_df.shape[1])
-
-        assert_array_almost_equal(
-            per_observation_df["Channel1 volume"], data_to_fit_media_data[:, 0], decimal=3
-        )
-        assert_array_almost_equal(
-            per_observation_df["Channel2 volume"], data_to_fit_media_data[:, 1], decimal=3
-        )
-        assert_array_almost_equal(
-            per_observation_df["Channel1 cost"], data_to_fit_costs_by_row_data[:, 0], decimal=3
-        )
-        assert_array_almost_equal(
-            per_observation_df["Channel2 cost"], data_to_fit_costs_by_row_data[:, 1], decimal=3
-        )
-
-        self.assertAlmostEqual(
-            data_to_fit_media_data[1, 1], per_observation_df.loc["2022-01-02", "Channel2 volume"]
-        )
-
-        assert_array_almost_equal(
-            per_observation_df["Feature1"], data_to_fit_extra_features_data[:, 0], decimal=3
-        )
-        assert_array_almost_equal(
-            per_observation_df["Feature2"], data_to_fit_extra_features_data[:, 1], decimal=3
-        )
-        assert_array_almost_equal(
-            per_observation_df["Feature3"], data_to_fit_extra_features_data[:, 2], decimal=3
-        )
-        assert_array_almost_equal(per_observation_df["Target"], data_to_fit_target_data, decimal=3)
-
-        self.assertEqual(2, per_channel_df.shape[0])
-        assert_array_almost_equal(per_channel_df["Cost"], data_to_fit.media_costs_scaled, decimal=3)
-
-        # unscaled = True
-        per_observation_df, per_channel_df = data_to_fit.to_data_frame(unscaled=True)
-
-        self.assertEqual(100, per_observation_df.shape[0])
-        self.assertEqual(2 + 2 + 3 + 1, per_observation_df.shape[1])
-
-        assert_array_almost_equal(
-            per_observation_df["Channel1 volume"], input_data.media_data[:, 0], decimal=3
-        )
-        assert_array_almost_equal(
-            per_observation_df["Channel2 volume"], input_data.media_data[:, 1], decimal=3
-        )
-        assert_array_almost_equal(
-            per_observation_df["Channel1 cost"], input_data.media_costs_by_row[:, 0], decimal=2
-        )
-        assert_array_almost_equal(
-            per_observation_df["Channel2 cost"], input_data.media_costs_by_row[:, 1], decimal=2
-        )
-
-        self.assertAlmostEqual(
-            input_data.media_data[1, 1],
-            per_observation_df.loc["2022-01-02", "Channel2 volume"],
-            places=3,
-        )
-
-        assert_array_almost_equal(
-            per_observation_df["Feature1"], input_data.extra_features_data[:, 0], decimal=3
-        )
-        assert_array_almost_equal(
-            per_observation_df["Feature2"], input_data.extra_features_data[:, 1], decimal=3
-        )
-        assert_array_almost_equal(
-            per_observation_df["Feature3"], input_data.extra_features_data[:, 2], decimal=3
-        )
-        assert_array_almost_equal(per_observation_df["Target"], input_data.target_data, decimal=3)
-
-        self.assertEqual(2, per_channel_df.shape[0])
-        assert_array_almost_equal(per_channel_df["Cost"], input_data.media_costs, decimal=3)
+        self.assertIsInstance(input_data, InputData)
 
     def test_clone_as_weekly(self):
         # un-even number of observations
-        input_data = ModelTestCase._generate_test_input_data(observations=100)
+        input_data = InputDataTest.generate_test_input_data(observations=100)
         input_data_weekly = input_data.clone_as_weekly()
 
         self.assertEqual(14, input_data_weekly.date_strs.shape[0])
@@ -213,7 +93,7 @@ class ModelTestCase(unittest.TestCase):
         )
 
         # even number of observations
-        input_data = ModelTestCase._generate_test_input_data(observations=98)
+        input_data = InputDataTest.generate_test_input_data(observations=98)
         input_data_weekly = input_data.clone_as_weekly()
 
         self.assertEqual(14, input_data_weekly.date_strs.shape[0])
@@ -239,7 +119,7 @@ class ModelTestCase(unittest.TestCase):
         self.assertAlmostEqual(input_data.target_data.sum(), input_data_weekly.target_data.sum())
 
     def test_clone_and_add_extra_features(self):
-        input_data = ModelTestCase._generate_test_input_data(observations=100)
+        input_data = InputDataTest.generate_test_input_data(observations=100)
 
         # 100 observations * 2 features
         new_feature_data = (np.arange(100 * 2).astype(np.float64) * 30.0).reshape(100, 2)
@@ -254,7 +134,7 @@ class ModelTestCase(unittest.TestCase):
         assert_array_almost_equal(new_feature_data, new_input_data.extra_features_data[:, 3:5])
 
     def test_clone_and_split_media_data(self):
-        input_data = ModelTestCase._generate_test_input_data(observations=100)
+        input_data = InputDataTest.generate_test_input_data(observations=100)
 
         input_data_split_channel_0 = input_data.clone_and_split_media_data(
             channel_idx=0,
