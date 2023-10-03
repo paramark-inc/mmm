539499b517ae4fc55045b75196c469d4047bc641
Hourann Bosci <azza-bazoo@users.noreply.github.com>
2023-10-03 12:58:38 -0700

    allow passing directory name to make_results_dir(), don't always generate a key

--- mmm/store/store.py
+++ mmm/store/store.py
@@ -7,7 +7,15 @@ from impl.lightweight_mmm.lightweight_mmm.utils import load_model as lwmmm_load_
 from impl.lightweight_mmm.lightweight_mmm.utils import save_model as lwmmm_save_model
 
 
-def make_results_dir(data_dir: str, dirname_fixed: str = "") -> str:
+def generate_unique_date_key():
+    now_date = datetime.now()
+    yyyymmdd = now_date.date().strftime("%Y-%m-%d")
+    seconds_since_midnight = now_date.hour * 3600 + now_date.minute * 60 + now_date.second
+    hextoken = secrets.token_hex(4)
+    return f"{yyyymmdd}-{seconds_since_midnight}-{hextoken}"
+
+
+def make_results_dir(data_dir: str, dirname_fixed: str = "", results_key=None) -> str:
     """
     create a directory with a unique name for this mmm run
 
@@ -15,13 +23,10 @@ def make_results_dir(data_dir: str, dirname_fixed: str = "") -> str:
     :param dirname_fixed: (optional) fixed path suffix, e.g. for a project name or customer name
     :return: directory name of form <data_dir>/results/<dirname_fixed>/<generated name>
     """
-    now_date = datetime.now()
-    yyyymmdd = now_date.date().strftime("%Y-%m-%d")
-    seconds_since_midnight = now_date.hour * 3600 + now_date.minute * 60 + now_date.second
-    hextoken = secrets.token_hex(4)
-    results_dir = os.path.join(
-        data_dir, "results", dirname_fixed, f"{yyyymmdd}-{seconds_since_midnight}-{hextoken}"
-    )
+    if results_key is None:
+        results_key = generate_unique_date_key()
+
+    results_dir = os.path.join(data_dir, "results", dirname_fixed, results_key)
     print(f"Creating directory '{results_dir}' for output")
     os.makedirs(results_dir, exist_ok=False)
 
