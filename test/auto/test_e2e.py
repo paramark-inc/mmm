4b914c06ca74f9322b97280a616ab0ef103c989b
Hourann Bosci <azza-bazoo@users.noreply.github.com>
2023-10-10 14:51:34 -0700

    update test from d5a27cc44974ef27e390555a1264451958b4a78e to account for jax rng variation across CPUs

--- test/auto/test_e2e.py
+++ test/auto/test_e2e.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import platform
 import tempfile
 import unittest
 from numpy.testing import assert_array_almost_equal
@@ -42,40 +43,43 @@ class EndToEndTest(unittest.TestCase):
             self.assertListEqual(os.listdir(tmpdirname), ["fit_params.yaml"])
 
             # then verify model fit by looking at random values from the trace
-            assert_array_almost_equal(
-                model.trace["ad_effect_retention_rate"][4], [0.8497401, 0.6917644]
-            )
-            assert_array_almost_equal(model.trace["coef_media"][8], [0.9149572, 0.12196853])
-            self.assertAlmostEqual(model.trace["coef_trend"][1][0], 0.04331071)
+            # note that even with a fixed seed, JAX's random number generator
+            # produces (deterministically) different results on different CPUs
+            if platform.machine() == "x86_64":
+                expected = {
+                    "coef_media": [1.6870707, 0.17920175],
+                    "expo_trend": 1.3377011,
+                    "gamma_seasonality": [[-0.885461, 0.07778881]],
+                    "intercept": 0.08363592,
+                    "peak_effect_delay": [0.23231119, 1.6421096],
+                    "prediction": [117.0781174, 108.2175751, 110.4735489, 114.1343918],
+                }
+            elif platform.machine() == "arm64":
+                expected = {
+                    "coef_media": [0.9149572, 0.12196853],
+                    "expo_trend": 1.2901471,
+                    "gamma_seasonality": [[-8.8577402e-01, -2.1908896e-03]],
+                    "intercept": 2.1729574,
+                    "peak_effect_delay": [0.05093075, 1.7051804],
+                    "prediction": [118.0228882, 125.4357605, 123.3989105, 150.4321747],
+                }
+
+            assert_array_almost_equal(model.trace["coef_media"][8], expected["coef_media"])
             self.assertAlmostEqual(
                 model.trace["expo_trend"][9],
-                1.2901471,
-            )
-            assert_array_almost_equal(
-                model.trace["exponent"][3],
-                [0.95118594, 0.9256289],
+                expected["expo_trend"],
             )
             assert_array_almost_equal(
                 model.trace["gamma_seasonality"][0],
-                [[-8.8577402e-01, -2.1908896e-03]],
-            )
-            self.assertAlmostEqual(model.trace["intercept"][2][0], 2.1729574)
-            assert_array_almost_equal(
-                model.trace["media_transformed"][5][2],
-                [3.0492800e-01, 4.8702326e-01],
+                expected["gamma_seasonality"],
             )
             self.assertAlmostEqual(
-                model.trace["mu"][2][2],
-                0.6117116,
+                model.trace["intercept"][2][0],
+                expected["intercept"],
             )
             assert_array_almost_equal(
                 model.trace["peak_effect_delay"][1],
-                [0.05093075, 1.7051804],
-            )
-            self.assertAlmostEqual(model.trace["sigma"][6][0], 0.24445353)
-            self.assertAlmostEqual(
-                model.trace["weekday"][7][3],
-                0.3071486,
+                expected["peak_effect_delay"],
             )
 
             # finally, verify that the model makes predictions as expected
@@ -87,6 +91,4 @@ class EndToEndTest(unittest.TestCase):
                 target_scaler=data_to_fit.target_scaler,
             )
 
-            assert_array_almost_equal(
-                prediction[9], [118.0228882, 125.4357605, 123.3989105, 150.4321747]
-            )
+            assert_array_almost_equal(prediction[9], expected["prediction"])
