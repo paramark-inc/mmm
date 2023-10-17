1f57527639a0e462669628ddac31c053ddec10ef
Pete Belknap <pete@paramark.com>
2023-10-17 14:20:33 -0700

    csv_to_df_generic: add ability to preserve ignore_cols

--- mmm/parser/csv.py
+++ mmm/parser/csv.py
@@ -4,7 +4,9 @@ import numpy as np
 import pandas as pd
 
 
-def _parse_csv_shared(data_fname: str, config: dict) -> pd.DataFrame:
+def _parse_csv_shared(
+    data_fname: str, config: dict, keep_ignore_cols: bool = False
+) -> pd.DataFrame:
     # allow config option for name of date column;
     # if not provided, use "date" (case sensitive)
     index_col = config.get("date_col", "date")
@@ -24,7 +26,7 @@ def _parse_csv_shared(data_fname: str, config: dict) -> pd.DataFrame:
     elif "to_use" in config["data_rows"]:
         data_df = data_df.tail(config["data_rows"]["to_use"])
 
-    if "ignore_cols" in config:
+    if not keep_ignore_cols and "ignore_cols" in config:
         data_df = data_df.drop(columns=config["ignore_cols"])
 
     return data_df
@@ -58,17 +60,20 @@ def parse_csv_generic(data_fname: str, config: dict):
     return data_dict
 
 
-def csv_to_df_generic(data_fname: str, config: dict) -> pd.DataFrame:
+def csv_to_df_generic(
+    data_fname: str, config: dict, keep_ignore_cols: bool = False
+) -> pd.DataFrame:
     """
     Parse a CSV to a DataFrame (generic implementation).
 
     Args:
         data_fname: full path to file for raw data
         config: config dictionary (from yaml file)
+        keep_ignore_cols: True to add the ignore_cols to the dataframe, False otherwise
 
     Returns:
         DataFrame
     """
-    data_df = _parse_csv_shared(data_fname, config)
+    data_df = _parse_csv_shared(data_fname, config, keep_ignore_cols)
     data_df.index = pd.DatetimeIndex(pd.to_datetime(data_df.index, format="%Y-%m-%d"), freq="D")
     return data_df
