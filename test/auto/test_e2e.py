ccc0720abea14523c415b2db46e20ea3193f564c
Hourann Bosci <azza-bazoo@users.noreply.github.com>
2023-10-20 09:59:49 -0700

    fix the model fit test by checking shapes; values was a folly :confounded:

--- test/auto/test_e2e.py
+++ test/auto/test_e2e.py
@@ -1,9 +1,7 @@
 import numpy as np
 import os
-import platform
 import tempfile
 import unittest
-from numpy.testing import assert_array_almost_equal
 
 from base_driver.config import load_config
 from mmm.data import DataToFit
@@ -42,47 +40,24 @@ class EndToEndTest(unittest.TestCase):
             self.assertEqual(model.n_media_channels, 2)
             self.assertListEqual(os.listdir(tmpdirname), ["fit_params.yaml"])
 
-            # then verify model fit by looking at random values from the trace
-            # note that even with a fixed seed, JAX's random number generator
-            # produces (deterministically) different results on different CPUs
-            if platform.machine() == "x86_64":
-                expected = {
-                    "coef_media": [1.6870707, 0.17920175],
-                    "expo_trend": 1.3377011,
-                    "gamma_seasonality": [[-0.885461, 0.07778881]],
-                    "intercept": 0.08363592,
-                    "peak_effect_delay": [0.23231119, 1.6421096],
-                    "prediction": [117.0781174, 108.2175751, 110.4735489, 114.1343918],
-                }
-            elif platform.machine() == "arm64":
-                expected = {
-                    "coef_media": [0.9149572, 0.12196853],
-                    "expo_trend": 1.2901471,
-                    "gamma_seasonality": [[-8.8577402e-01, -2.1908896e-03]],
-                    "intercept": 2.1729574,
-                    "peak_effect_delay": [0.05093075, 1.7051804],
-                    "prediction": [118.0228882, 125.4357605, 123.3989105, 150.4321747],
-                }
+            # then verify model fit by looking at shapes of objects in the trace
+            # (we can't look at values directly because even with a fixed seed,
+            # JAX's RNG behaves differently on different CPU architectures)
+            self.assertEqual(model.trace["ad_effect_retention_rate"].shape, (10, 2))
+            self.assertEqual(model.trace["coef_media"].shape, (10, 2))
+            self.assertEqual(model.trace["coef_trend"].shape, (10, 1))
+            self.assertEqual(model.trace["expo_trend"].shape, (10,))
+            self.assertEqual(model.trace["exponent"].shape, (10, 2))
+            self.assertEqual(model.trace["gamma_seasonality"].shape, (10, 1, 2))
+            self.assertEqual(model.trace["intercept"].shape, (10, 1))
+            self.assertEqual(model.trace["media_transformed"].shape, (10, 9, 2))
+            self.assertEqual(model.trace["mu"].shape, (10, 9))
+            self.assertEqual(model.trace["peak_effect_delay"].shape, (10, 2))
+            self.assertEqual(model.trace["sigma"].shape, (10, 1))
+            self.assertEqual(model.trace["weekday"].shape, (10, 7))
 
-            assert_array_almost_equal(model.trace["coef_media"][8], expected["coef_media"])
-            self.assertAlmostEqual(
-                model.trace["expo_trend"][9],
-                expected["expo_trend"],
-            )
-            assert_array_almost_equal(
-                model.trace["gamma_seasonality"][0],
-                expected["gamma_seasonality"],
-            )
-            self.assertAlmostEqual(
-                model.trace["intercept"][2][0],
-                expected["intercept"],
-            )
-            assert_array_almost_equal(
-                model.trace["peak_effect_delay"][1],
-                expected["peak_effect_delay"],
-            )
-
-            # finally, verify that the model makes predictions as expected
+            # finally, check that the model makes predictions as expected, with
+            # another shape check, and a loose check on the output values
             prediction_input = np.array([[11, 0], [12, 1], [13, 0], [14, 1]], np.float32)
 
             prediction = model.predict(
@@ -91,4 +66,12 @@ class EndToEndTest(unittest.TestCase):
                 target_scaler=data_to_fit.target_scaler,
             )
 
-            assert_array_almost_equal(prediction[9], expected["prediction"])
+            self.assertEqual(prediction.shape, (10, 4))
+
+            mean_predictions = prediction.mean(axis=1)
+            assert np.all(
+                mean_predictions > 50
+            ), f"expected prediction means greater than 50, got: {mean_predictions}"
+            assert np.all(
+                mean_predictions < 200
+            ), f"expected prediction means less than 200, got: {mean_predictions}"
