2ffecdda761ba5de4ae62d2156742519eb271d4f
Jason Gonzales <jason@paramark.com>
2025-04-22 14:21:50 -0700

    do not generate fit_data.csv if we have a hierarchical model

--- mmm/describe/describe.py
+++ mmm/describe/describe.py
@@ -900,14 +900,15 @@ def describe_mmm_training(
         baseline = None
 
     # Get and save fit data
-    fit_df = get_fit_in_sample_df(
-        media_mix_model=mmm,
-        data_to_fit=data_to_fit,
-        input_data=input_data,
-        geo_name=None,  # For now, we don't support geo filtering in describe_mmm_training
-        time_granularity=None,  # Use original time granularity
-    )
-    fit_df.to_csv(os.path.join(results_dir, "fit_data.csv"))
+    if data_to_fit.geo_names is None:
+        fit_df = get_fit_in_sample_df(
+            media_mix_model=mmm,
+            data_to_fit=data_to_fit,
+            input_data=input_data,
+            geo_name=None,  # For now, we don't support geo filtering in describe_mmm_training
+            time_granularity=None,  # Use original time granularity
+        )
+        fit_df.to_csv(os.path.join(results_dir, "fit_data.csv"))
 
     # Get and save media and baseline contribution data
     media_baseline_df = get_media_and_baseline_contribution_df(
